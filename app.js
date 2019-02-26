ff3ca55f9c32ec8459aef9eba39bf80e3a25590d
Avinash Dhillor <avinashdhillor24@gmail.com>
2019-02-26 08:55:02 +0530

    yargs setup done

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-const dotenv = require(dotenv).config();
+const dotenv = require("dotenv").config();
 const request = require("request");
+const yargs = require("yargs");
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Addtrdd to fetch weather for",
+      string: true
+    }
+  })
+  .help()
+  .alias("help", "h").argv;
+
+console.log(argv);
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request(
   {
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%lombard%street%philadelphia&key=${prcoessKEY}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${
+      process.env.KEY
+    }`,
     json: true
   },
   (error, response, body) => {
